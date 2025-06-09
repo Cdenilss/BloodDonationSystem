eb915027c592f57fc09652d4c994a0d6e035f724
Cdenilss <carlosdenilss@gmail.com>
2025-06-09 17:09:11 -0300

    feat: inject ViaCep service into CreateDonorHandler for address retrieval

--- BloodDonationSystem.Application/Commands/DonorsCommand/Insert/CreateDonorHandler.cs
+++ BloodDonationSystem.Application/Commands/DonorsCommand/Insert/CreateDonorHandler.cs
@@ -1,5 +1,6 @@
 using BloodDonationSystem.Application.Common.Mediator;
 using BloodDonationSystem.Application.Models.ResultViewModel;
+using BloodDonationSystem.Application.Services.ViaCep;
 using BloodDonationSystem.Core.Repositories;
 
 namespace BloodDonationSystem.Application.Commands.DonorsCommand.Insert;
@@ -7,10 +8,12 @@ namespace BloodDonationSystem.Application.Commands.DonorsCommand.Insert;
 public class CreateDonorHandler : IRequestHandler<CreateDonorCommand, ResultViewModel<Guid>>
 {
     private readonly IRepositoryDonor _donorRepository;
+    private readonly IViaCepService _viaCepService;
 
-    public CreateDonorHandler(IRepositoryDonor donorRepository)
+    public CreateDonorHandler(IRepositoryDonor donorRepository, IViaCepService viaCepService)
     {
         _donorRepository = donorRepository;
+        _viaCepService = viaCepService;
     }
 
     public async Task<ResultViewModel<Guid>> Handle(CreateDonorCommand request, CancellationToken cancellationToken)
