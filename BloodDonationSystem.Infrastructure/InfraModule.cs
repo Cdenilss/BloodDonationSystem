fcffa2b04ebdda352c798bbd03a71c5e6d37f913
Cdenilss <carlosdenilss@gmail.com>
2025-06-09 16:48:16 -0300

    feat: add database configuration and context registration in InfraModule

--- BloodDonationSystem.Infrastructure/InfraModule.cs
+++ BloodDonationSystem.Infrastructure/InfraModule.cs
@@ -1,5 +1,7 @@
 using BloodDonationSystem.Core.Repositories;
+using BloodDonationSystem.Infrastructure.Persistence;
 using BloodDonationSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,9 +12,18 @@ public static class InfraModule
     public static IServiceCollection AddInfrastrucutre(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddRepositoryies();
+        services.AddDatabase(configuration);
         return services;
     }
     
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        var connectionString = configuration.GetConnectionString("BloodDonationDb");
+        services.AddDbContext<BloodDonationDbContext>(o => o.UseSqlServer(connectionString));
+        return services;
+        
+    }
     public static IServiceCollection AddRepositoryies(this IServiceCollection services)
     {
         services.AddScoped<IRepositoryDonor,DonorRepository>();
