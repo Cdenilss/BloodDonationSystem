52d698296ac2d4b7f8a3dd22672e86556105bea4
Cdenilss <carlosdenilss@gmail.com>
2025-06-13 17:46:34 -0300

    feat: add AddressMappings to convert ViaCepAddressResponse to Address entity

---
+++ BloodDonationSystem.Application/Mappings/AddressMappings.cs
@@ -0,0 +1,22 @@
+using BloodDonationSystem.Application.Services.ViaCep;
+using BloodDonationSystem.Core.Entities;
+
+namespace BloodDonationSystem.Application.Mappings;
+
+public static class AddressMappings
+{
+    
+    public static Address ToEntity(this ViaCepAddressResponse viaCepResponse, string number, string? complement)
+    {
+        return new Address(
+            street: viaCepResponse.Logradouro ?? string.Empty,
+            city: viaCepResponse.Localidade ?? string.Empty,
+            state: viaCepResponse.Uf ?? string.Empty,
+            zipCode: viaCepResponse.Cep?.Replace("-","") ?? string.Empty,
+            number: number,
+            complement: complement,
+            district: viaCepResponse.Bairro ?? string.Empty
+        );
+    }
+}
+
