e22212217c4c3300c39d6b91a101fe2d17f54229
Cdenilss <carlosdenilss@gmail.com>
2025-06-13 17:44:14 -0300

    feat: extend Address class to include number, complement, and district properties

--- BloodDonationSystem.Core/Entities/Address.cs
+++ BloodDonationSystem.Core/Entities/Address.cs
@@ -2,14 +2,19 @@ namespace BloodDonationSystem.Core.Entities;
 
 public class Address : BaseEntity
 {
-    public Address(string street, string city, string state, string zipCode)
+    public Address(string street, string city, string state, string zipCode, string number, string? complement, string district)
+        : base()
     {
         Street = street;
         City = city;
         State = state;
         ZipCode = zipCode;
+        Number = number;
+        Complement = complement;
+        District = district;
         
     }
+    
     protected Address() { }
 
  
@@ -17,7 +22,11 @@ public class Address : BaseEntity
     public string City { get; private set; }
     public string State { get; private  set; }
     public string ZipCode { get; private  set; }
-    
+
+    public string? Number { get; private set; }
+    public string? Complement { get; private set; }
+    public string District { get; private set;}
+
     public Guid DonorId { get; private  set; }
     public Donor? Donor { get; private  set; }
     
