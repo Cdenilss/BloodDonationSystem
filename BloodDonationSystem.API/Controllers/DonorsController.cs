ef1d26a1aaf867a35153256e1261fac0c1ce9c5b
Cdenilss <carlosdenilss@gmail.com>
2025-06-13 17:47:14 -0300

    feat: implement GetDonorById and AddDonor methods in DonorsController with mediator pattern

--- BloodDonationSystem.API/Controllers/DonorsController.cs
+++ BloodDonationSystem.API/Controllers/DonorsController.cs
@@ -1,3 +1,8 @@
+using BloodDonationSystem.Application.Commands.DonorsCommand.Insert;
+using BloodDonationSystem.Application.Common.Mediator;
+using BloodDonationSystem.Application.Models.DTO;
+using BloodDonationSystem.Application.Models.ResultViewModel;
+using BloodDonationSystem.Application.Queries.DonorsQueries.GetById;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloodDonationSystem.Controllers;
@@ -7,6 +12,11 @@ namespace BloodDonationSystem.Controllers;
 
 public class DonorsController: ControllerBase
 {
+    private readonly IMediator _mediator;
+    public DonorsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
     [HttpGet("GetAllDonors")]
     
     public async Task<IActionResult> GetAllDonors()
@@ -14,4 +24,34 @@ public class DonorsController: ControllerBase
         return Ok();
     }
     
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDonorById(Guid id)
+    {
+      
+        var result = await _mediator.SendWithResponse(new GetDonorByIdQuery(id));
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest();
+        }
+            return Ok(result);
+    }
+        
+    
+    
+    [HttpPost]
+    public async Task<IActionResult> AddDonor(CreateDonorCommand command)
+    {
+        var result = await _mediator.SendWithResponse(command);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Errors);
+        }
+        
+        return CreatedAtAction(nameof(GetDonorById), new { id = result.Data }, command);
+    }
+    
 }
+
