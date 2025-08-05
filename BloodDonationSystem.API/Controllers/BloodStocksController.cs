8390f77263044ffdcecf829d6090dd7acaced036
Cdenilss <carlosdenilss@gmail.com>
2025-08-05 18:24:39 -0300

    feat: implement GetAllBloodStocks and BloodDraw endpoints in BloodStocksController

--- BloodDonationSystem.API/Controllers/BloodStocksController.cs
+++ BloodDonationSystem.API/Controllers/BloodStocksController.cs
@@ -1,15 +1,47 @@
+using BloodDonationSystem.Application.Commands.BloodStockPutCommand.OutPut;
+using BloodDonationSystem.Application.Common.Mediator;
+using BloodDonationSystem.Application.Queries.BloodSrocksQueries;
+using BloodDonationSystem.Core.Enum;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloodDonationSystem.Controllers;
 
 [Route("api/bloodstocks")]
 [ApiController]
-public class BloodStocksController: ControllerBase
+public class BloodStocksController : ControllerBase
 {
-    [HttpGet("GetBloodStocks")]
-    
-    public async Task<IActionResult> GetAllDonors()
+    private readonly IMediator _mediator;
+
+    public BloodStocksController(IMediator mediator)
     {
-        return Ok();
+        _mediator = mediator;
     }
-}
+
+    [HttpGet("GetAllBloodStocks")]
+    public async Task<IActionResult> GetAllBloodStocks()
+    {
+
+        var result = await _mediator.SendWithResponse(new GetAllBloodStocksQuery());
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok(result.Data);
+    }
+
+    [HttpPut("bloodDraw")]
+        public async Task<IActionResult> BloodDraw(OutputBloodStockCommand command)
+        {
+            var result = await _mediator.SendWithResponse(command);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return NoContent();
+        }
+    }
+
+
