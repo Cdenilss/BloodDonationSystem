ab6e688b7feb9e32d2303304fdc164be622c22db
Cdenilss <carlosdenilss@gmail.com>
2025-08-06 22:37:13 -0300

    Refec: implemnta o Domain Events no dbcontex

--- BloodDonationSystem.Infrastructure/Persistence/BloodDonationDbContext.cs
+++ BloodDonationSystem.Infrastructure/Persistence/BloodDonationDbContext.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using BloodDonationSystem.Application.Common.Mediator;
+using BloodDonationSystem.Core.Common;
+using BloodDonationSystem.Core.DomainEvents;
 using BloodDonationSystem.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +9,15 @@ namespace BloodDonationSystem.Infrastructure.Persistence;
 
 public class BloodDonationDbContext : DbContext
 {
+   private readonly IMediator _mediator;
+
     protected BloodDonationDbContext()
     {
     }
 
-    public BloodDonationDbContext(DbContextOptions options) : base(options)
+    public BloodDonationDbContext(DbContextOptions options, IMediator mediator) : base(options)
     {
+       _mediator = mediator;
     }
     
     
@@ -25,4 +31,36 @@ public class BloodDonationDbContext : DbContext
 
         base.OnModelCreating(modelBuilder);
     }
-}
+    
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        
+        int result= await base.SaveChangesAsync(cancellationToken); 
+        
+        await PublishDomainEvents();
+        
+        return result;
+    }
+    
+    private async Task PublishDomainEvents()
+    {
+        var domainEntities = ChangeTracker
+            .Entries<IAggregateRoot>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity)
+            .ToList();
+
+        var domainEvents = domainEntities
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _mediator.PublishDomainEvent(domainEvent); // método custom
+        }
+    }
+   
+    }
+    
+   
+    
