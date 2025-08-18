79c900b35c7dccb3ca09ab804f3ee864ef94be52
Cdenilss <carlosdenilss@gmail.com>
2025-08-18 17:45:05 -0300

    refactor: simplify OutputBloodStockCommandHandler by using inline property assignment and method extraction

--- BloodDonationSystem.Application/Commands/BloodStockPutCommand/OutPut/OutputBloodStockCommandHandler.cs
+++ BloodDonationSystem.Application/Commands/BloodStockPutCommand/OutPut/OutputBloodStockCommandHandler.cs
@@ -7,26 +7,15 @@ namespace BloodDonationSystem.Application.Commands.BloodStockPutCommand.OutPut;
 public class OutputBloodStockCommandHandler : IRequestHandler<OutputBloodStockCommand, ResultViewModel>
 {
     private readonly IUnitOfWork _unitOfWork;
-    public OutputBloodStockCommandHandler(IUnitOfWork unitOfWork)
-    {
-        _unitOfWork = unitOfWork;
-    }
+    public OutputBloodStockCommandHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
 
     public async Task<ResultViewModel> Handle(OutputBloodStockCommand request, CancellationToken cancellationToken)
     {
-            
-            
-        var bloodStock = await _unitOfWork.BloodStocks.GetByTypeAsync(request.BloodType, request.RhFactor);
-        if (bloodStock == null)
-        {
-            return ResultViewModel.Error("Blood stock not found.");
-        }
-            
-        bloodStock.QuantityMl -= request.QuantityMl;
-        _unitOfWork.BloodStocks.UpdateAsync(bloodStock);
-            
-        await _unitOfWork.CompleteAsync();
+        var stock = await _unitOfWork.BloodStocks.GetByTypeAsync(request.BloodType, request.RhFactor);
+        if (stock is null) return ResultViewModel.Error("Blood stock not found.");
+
+        stock.Draw(request.QuantityMl);              
+        await _unitOfWork.CompleteAsync();           
         return ResultViewModel.Success();
-           
     }
 }
