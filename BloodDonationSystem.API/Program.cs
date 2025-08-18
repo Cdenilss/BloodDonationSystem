07dbc0af280071e854368f99e31e784d5fed88ae
Cdenilss <carlosdenilss@gmail.com>
2025-08-18 17:45:13 -0300

    fix: reorder service registrations for improved application setup

--- BloodDonationSystem.API/Program.cs
+++ BloodDonationSystem.API/Program.cs
@@ -18,8 +18,9 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 
 
 var app = builder.Build();
