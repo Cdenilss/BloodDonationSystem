ff584235bbced000d4a30149c1bfb77482ee8f42
Cdenilss <carlosdenilss@gmail.com>
2025-08-18 17:46:41 -0300

    feat: add BloodStockReport class for generating detailed blood stock reports

---
+++ BloodDonationSystem.Infrastructure/Reports/BloodSotckReport.cs
@@ -0,0 +1,45 @@
+using BloodDonationSystem.Application.Models.DTO;
+using BloodDonationSystem.Core.Enum;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+public class BloodStockReport : IDocument
+{
+    public IReadOnlyList<BloodStockItemViewModel> Stocks { get; }
+
+    public BloodStockReport(IReadOnlyList<BloodStockItemViewModel> stocks)
+    {
+        Stocks = stocks;
+        QuestPDF.Settings.License = LicenseType.Community;
+        
+    }
+
+    public void Compose(IDocumentContainer container)
+    {
+        container.Page(page =>
+        {
+            page.Margin(30);
+
+            page.Header()
+                .AlignCenter()
+                .Text("Blood Stock Report")
+                .FontSize(20)
+                .SemiBold();
+
+            page.Content().Column(col =>
+            {
+                foreach (var item in Stocks)
+                {
+                    var typeLabel = $"Tipo sanguíneo {item.BloodType}{(item.RhFactor == RhFactorEnum.Positive ? "+" : "-")}";
+                    col.Item().Text($"{typeLabel} : {item.QuantityMl} ml").FontSize(14);
+                }
+
+                col.Item().PaddingTop(10).Text($"Total geral: {Stocks.Sum(s => s.QuantityMl)} ml").SemiBold();
+            });
+
+            page.Footer()
+                .AlignRight()
+                .Text($"Generated at {DateTime.Now:dd/MM/yyyy HH:mm}");
+        });
+    }
+}
