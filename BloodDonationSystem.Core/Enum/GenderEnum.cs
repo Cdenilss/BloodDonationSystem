5153fd4c504de93084200a293bc658d86f550270
Cdenilss <carlosdenilss@gmail.com>
2025-08-20 18:38:00 -0300

    fix: correct spelling of 'Female' in GenderEnum

--- BloodDonationSystem.Core/Enum/GenderEnum.cs
+++ BloodDonationSystem.Core/Enum/GenderEnum.cs
@@ -2,8 +2,8 @@ namespace BloodDonationSystem.Core.Enum;
 
 public enum GenderEnum
 {
-    Male=1,
-    Famale=2,
-    Other=3,
-    PreferNotToSay= 4
+    Male = 1,
+    Female = 2,
+    Other = 3,
+    PreferNotToSay = 4
 }
