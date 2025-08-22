4ee0a2f779195dabb294ac2cba70777dad6f72f6
Cdenilss <carlosdenilss@gmail.com>
2025-08-22 15:17:30 -0300

    fix: encapsulate BloodType and RhFactor properties for better data integrity

--- BloodDonationSystem.Core/Entities/BloodStock.cs
+++ BloodDonationSystem.Core/Entities/BloodStock.cs
@@ -13,21 +13,21 @@ public class BloodStock : BaseEntity
         MinimumSafeQuantity = 1500;
     }
 
-    public BloodTypeEnum BloodType { get; set; }
-    public RhFactorEnum RhFactor { get; set; }
+    public BloodTypeEnum BloodType { get; private set; }
+    public RhFactorEnum RhFactor { get; private set; }
     public int QuantityMl { get; set; }
     public int MinimumSafeQuantity { get; set; }
 
 
     public void Draw(int ml)
     {
-        QuantityMl -= ml;
-
         if (ml <= 0)
             throw new ArgumentException("A quantidade retirada deve ser maior que zero.", nameof(ml));
 
         if (ml > QuantityMl)
             throw new InvalidOperationException("Não é possível retirar mais sangue do que há em estoque.");
+
+        QuantityMl -= ml;
 
         if (QuantityMl <= MinimumSafeQuantity)
         {
